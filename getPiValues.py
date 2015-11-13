a0a1176487bafb9df6cbe7d15f7342a4c5711091
Xerxes0198 <greg.scott@y7mail.com>
2015-11-13 13:01:12 +0000

    Added get Pi Values

---
+++ getPiValues.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+def getIPAddress():
+	arg = "ip route list"
+	p = subprocess.Popen(arg, shell = True, stdout = subprocess.PIPE)
+	data = p.communicate()
+	split_data = data[0].split()	
+	ipaddr = split_data[split_data.index('src') + 1]
+	return ipaddr
+
+
+def getCPUTemp():
+	try:
+		s = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
+		return float(s.split('=')[1][:-3])
+	except:
+		return 0

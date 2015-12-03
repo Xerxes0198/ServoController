e926ce369265f78e4c2b899d568e182718319350
Greg <greg.scott@y7mail.com>
2015-12-03 20:27:46 +1100

    Updates
    555

--- nodeDataReader.js
+++ nodeDataReader.js
@@ -7,7 +7,7 @@ var inputFileName = 'piValues.txt';
 var dataReaderEnabled = true;
 
 //Create servo values
-var FILE_STEERING_SERVO = '/servoValues/steering_servo';
+var SERVO_FOLDER = 'servoValues/';
 
 function modLog(message)
 {
@@ -28,52 +28,12 @@ this.writeSteeringValue = function(newVal)
   fs.writeFile(servoFileName, newVal);
 }
 
-//Functions to read values
-this.readSteeringValue = function()
-{
-  //Return steering valie from file
-  //Format of <Io:VALUE>
-
-  //Check file exists
-  //  Try
-  //    Read in values and split on ':'
-  //  try converting to JSON as {pin:"<PIN NUMBER>", value:"<Read Value>"}
-  //Return JSON object, or return null!
-}
-
-this.readThrottleValue = function()
-{
-  //Return throttle valie from file
-  //Format of <Io:VALUE>
-
-  //Convert to JSON
-
-  //Return JSON
-}
-
+//Read all servo values and create a servo object
 this.readInitData = function()
 {
-  modLog("Reading initial JSON data from python server...")
-
-  fs.readFile(inputFileName, function(error, data)
+  files = fs.readdirSync(SERVO_FOLDER).forEach(function(file)
   {
-    if(error != null)
-    {
-      modLog("Logging error from piValues read: " + error);
-      modLog("Can't find input data from node server!")
-      modLog("Disabling data reader module")
-
-      //Disable this module as no input will work
-      dataReaderEnabled = false;
-    }
-    else
-    {
-      modLog("Data Reader initialized correctly...");
-      modLog("Logging data from piValues read: " + data);
-    }
-
-    //Read in the current values of the servos
-
+    modLog(fs.readFileSync(SERVO_FOLDER + file.replace('\n', '')));
   });
 }
 
